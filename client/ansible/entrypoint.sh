076d0a6b886e9a32d3ceaefb05b8ff8e6b1d60cc
Rafal Lukawski <rs.lukawski@gmail.com>
2024-05-11 00:09:50 +0200

    add ansible openvpn

---
+++ client/ansible/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+echo "nameserver 8.8.8.8" >/etc/resolv.conf
+cd /etc/openvpn/client
+openvpn --config /etc/openvpn/client/client.conf --daemon
